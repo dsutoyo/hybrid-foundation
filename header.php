5af7c86e66d3d42ba4e5ee8b6cc1c033c704028d
David Sutoyo <sutoyod@yahoo.com>
2015-02-05 14:34:16 -0800

    Fix offcanvas for iOS

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 
 			<nav class="tab-bar show-for-small">
 				<section class="left-small">
-					<a class="left-off-canvas-toggle menu-icon"><span></span></a>
+					<a class="left-off-canvas-toggle menu-icon" href="#off-canvas-navigation" role="button"><span></span></a>
 				</section>
 				<section class="middle tab-bar-section">
 					<?php bloginfo( 'name' ); ?>
