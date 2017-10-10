543b5c91b546f1258bf6f6916d9e67211e6cdf3d
David Sutoyo <sutoyod@yahoo.com>
2017-10-09 18:08:13 -0700

    Remove references to popup nav

--- assets/javascripts/foundation.init.js
+++ assets/javascripts/foundation.init.js
@@ -1,15 +1,2 @@
 // Initialize Foundation
 jQuery(document).foundation();
-
-/*
- * Popup Nav
- */
-jQuery('.popup-navigation-trigger').on('click', function(e) {
-	jQuery('body').addClass('popup-active');
-	e.preventDefault();
-});
-
-jQuery('.popup-navigation-close').on('click', function(e) {
-	jQuery('body').removeClass('popup-active');
-	e.preventDefault();
-});
